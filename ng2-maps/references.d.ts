5b02951ccbea37edc549869d81511cfaa3945b57
Craig Doremus <cdoremus@gmail.com>
2016-10-12 14:39:18 -0400

    Added typings file for Android SDK

--- ng2-maps/references.d.ts
+++ ng2-maps/references.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" /> Needed for autocompletion and compilation.
 /// <reference path="./node_modules/nativescript-google-maps-sdk/map-view.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/tns-core-modules/android17.d.ts" />
 
